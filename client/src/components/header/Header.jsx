85b3cae1eabe8589060ee1317564299482da4367
Jaspreet <86220740+JaspreetRatta@users.noreply.github.com>
2022-03-28 19:08:01 +0700

    commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://i.pinimg.com/originals/39/bc/08/39bc0877b83e58cb4511fe5c10718c56.png"
+        alt=""
+      />
+    </div>
+  );
+}
